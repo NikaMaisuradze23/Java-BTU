4810fdb7fe29e233f9a5210d28326814792121d3
Nika Maisuradze <tudula6@gmail.com>
2022-10-10 21:00:27 +0400

    test2

--- L2/src/test1.java
+++ L2/src/test1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 
 public class test1 {
-    static void myMethod() {
+    static void method1() {
         Scanner num1 = new Scanner(System.in);
         Scanner num2 = new Scanner(System.in);
         String a = num1.nextLine();
@@ -11,7 +11,33 @@ public class test1 {
         System.out.println("Second: " + b);
     }
 
+    static void method2() {
+        System.out.println("დაწერეთ პირველი რიცხვი: ");
+        Scanner num1 = new Scanner(System.in);
+
+        System.out.println("დაწერეთ მეორე რიცხვი: ");
+        Scanner num2 = new Scanner(System.in);
+        int a = num1.nextInt();
+        int b = num2.nextInt();
+        System.out.println(b + a);
+
+    }
+
+    static void method3() {
+        System.out.println("დაწერეთ პირველი რიცხვი: ");
+        Scanner num1 = new Scanner(System.in);
+
+        System.out.println("დაწერეთ მეორე რიცხვი: ");
+        Scanner num2 = new Scanner(System.in);
+        int a = num1.nextInt();
+        int b = num2.nextInt();
+        System.out.println(b * a);
+
+    }
+
     public static void main(String[] args) {
-        myMethod();
+        method1();
+        method2();
+        method3();
     }
 }
